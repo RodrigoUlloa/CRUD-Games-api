0b00031c934368a5fd5ef78ef6b7f868b9a228e7
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-06-20 12:37:55 -0400

    GameException not exist

--- game-service-api/src/main/java/com/example/gameserviceapi/services/GameService.java
+++ game-service-api/src/main/java/com/example/gameserviceapi/services/GameService.java
@@ -1,7 +1,10 @@
 package com.example.gameserviceapi.services;
 
+import com.example.gameserviceapi.commons.constants.TopicConstants;
 import com.example.gameserviceapi.entities.Game;
 import com.example.gameserviceapi.repositories.GameRepository;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,16 +12,26 @@ import java.util.Optional;
 @Service
 public class GameService {
     private final GameRepository gameRepository;
+    private final StreamBridge streamBridge;
 
-    public GameService(GameRepository gameRepository) {
+    public GameService(GameRepository gameRepository, StreamBridge streamBridge) {
         this.gameRepository = gameRepository;
+        this.streamBridge = streamBridge;
     }
 
     public Game saveGame(String userId, Game gameRequest) {
         gameRequest.setUserId(Integer.parseInt(userId));
         return Optional.of(gameRequest)
                 .map(gameRepository::save)
-                .orElseThrow(() -> new RuntimeException("Error saving game"));
+                .map(this::sendGameEvent)
+                .orElseThrow(() -> new GameException(HttpStatus.BAD_REQUEST, "Error saving game"));
+    }
+
+    private Game sendGameEvent(Game game) {
+        Optional.of(game)
+                .map(givenGame -> this.streamBridge.send(TopicConstants.GAME_CREATED_TOPIC, game))
+                .map(bool -> game);
+        return game;
     }
 
     public Game updateGame(Game gameRequest){
